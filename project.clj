0bbffd8b5c8daa60f065c7ffeb70cd3d3ca1db80
Alexandre Gariépy <alexandre.gariepy@rakuten.com>
2019-11-27 16:00:53 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.curbside/curbside-clojure-ml "2.0.1"
+(defproject com.curbside/curbside-clojure-ml "2.1.0-SNAPSHOT"
   :description "Library for ML model training and serving."
   :url "http://github.com/RakutenReady/curbside-clojure-ml"
   :dependencies [[org.clojure/clojure "1.10.1"]
