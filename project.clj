8ff0838abaff5e332b8ee3bb232edb6f8a867198
Connor Clark <connor.clark@rakuten.com>
2020-03-24 11:07:35 -0700

    Version 2.4.2

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject com.curbside/curbside-clojure-ml "2.4.2-SNAPSHOT"
+(defproject com.curbside/curbside-clojure-ml "2.4.2"
   :description "Library for ML model training and serving."
   :url "http://github.com/RakutenReady/curbside-clojure-ml"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.0.567"]
                  [org.clojure/data.csv "0.1.4"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
                  [clj-time "0.15.2"]
                  [medley "1.2.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
