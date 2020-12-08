c0e2c8fa944fb2ba9e38164898ad5dc0623f8408
Alexandre Gariépy <alex.gariepy@curbside.com>
2020-12-08 12:13:40 -0500

    Fix regression when converting datasets to DMatrix
    https://github.com/RakutenReady/Team/issues/54141
    
    I was treating 0 values as missing values, which introduced a regression. If I
    recall correctly, I did that because I thought nil values were treated as zeros inside
    xgboost. So, not explicitly storing zeros inside the DMatrix would greatly
    reduce the memory footprint for in-store datasets.
    
    This differed from the previous behavior:
    
    https://github.com/RakutenReady/curbside-clojure-ml/blob/6c98101d6126cc5033edf03ec929d234793be0b8/src/curbside/ml/models/xgboost.clj#L31
    
    We might have to retrain IHOP models if there were trained against the 4.x.x major.

--- test/curbside/ml/models_test.clj
+++ test/curbside/ml/models_test.clj
@@ -2,6 +2,8 @@
   (:require
    [clojure.test :refer [deftest is testing]]
    [conjure.core :refer [stubbing verify-call-times-for verify-first-call-args-for]]
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
    [curbside.ml.data.dataset :as dataset]
    [curbside.ml.models :as models]
    [curbside.ml.utils.tests :as tutils]))
@@ -136,3 +138,32 @@
                   :subsample 0.8190881875862341
                   :booster "dart"}
                  optimal-params)))))))
+
+(deftest infer-regression-test
+  (testing "Verifies the value outputted by a regression ETA model. https://github.com/RakutenReady/Team/issues/54141"
+    (let [model (models/load :xgboost (.getPath (io/as-file (io/resource "models/eta/regression-test/5659.xgb"))))
+          factors (edn/read-string (slurp (io/resource "models/eta/regression-test/5659.scale.edn")))
+          config (edn/read-string (slurp (io/resource "models/eta/regression-test/5659.config.edn")))
+          feature-map {:average-approaching-speed-4 0.0,
+                       :cosine-angle -0.636894300348137,
+                       :site-lng -82.3596632480621,
+                       :time-of-day 16,
+                       :approaching-speed 0.0,
+                       :site-lat 34.8413076694805,
+                       :euclidean-distance 1769004.9423246796,
+                       :locations [{:lat 48.417492, :lng -71.151178, :ts "2019-02-18T15:35:07Z"}],
+                       :vertical-distance -1363816.3653824695,
+                       :line-distance 40875.06331285569,
+                       :sine-angle -0.7709511334605178,
+                       :lat 48.34444,
+                       :horizontal-distance -1126669.165054273,
+                       :lng -71.204523}
+          prediction (models/infer :xgboost :regression model
+                                   (get-in config [:features-selection :selected-features])
+                                   (get-in config [:train-models :hyperparameters])
+                                   feature-map
+                                   :scaling-factors factors
+                                   :feature-scaling-fns []
+                                   :label-scaling-fns [:log10]
+                                   :dataset-encoding nil)]
+      (is (= 777.2630419759837 prediction)))))
