e10eabfb00577009305688594f92649f0365cc75
Alexandre Gariépy <alex.gariepy@curbside.com>
2020-12-08 14:32:15 -0500

    Version 4.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.curbside/curbside-clojure-ml "4.1.2-SNAPSHOT"
+(defproject com.curbside/curbside-clojure-ml "4.1.2"
   :description "Library for ML model training and serving."
   :url "http://github.com/RakutenReady/curbside-clojure-ml"
   :dependencies [[org.clojure/clojure "1.10.1"]
