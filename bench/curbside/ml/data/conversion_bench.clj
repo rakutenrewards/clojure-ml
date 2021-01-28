31e231597907b337d1fa0ca646fddd135d6d5b31
Connor Clark <connor.clark@rakuten.com>
2021-01-28 17:07:19 -0500

    More features in bench

--- bench/curbside/ml/data/conversion_bench.clj
+++ bench/curbside/ml/data/conversion_bench.clj
@@ -4,12 +4,12 @@
             [curbside.ml.data.conversion :refer :all]))
 
 (defbench bench-feature-map-to-vector
-  (let [feature-names [:a :b :c]
+  (let [feature-names [:a :b :c :d :e :f :g :h]
         feature-encoding {:features
                           {:b {:type :one-hot
                                :encoding-size 3
                                :one-hot-indices {"foo" 0, "bar" 1, "spam" 2}}}}
-        feature-map {:a 1 :b "bar" :c 3}]
+        feature-map {:a 1 :b "bar" :c 3 :d 3 :e nil :g 1.1 :h 56}]
     (is (c/bench
          (doall
           (feature-map-to-vector feature-names feature-encoding feature-map))))))
