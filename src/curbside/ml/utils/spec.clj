86242ad967473acdb8f393f66a941391cde1496f
Alexandre Gariépy <alex.gariepy@curbside.com>
2021-02-02 10:17:52 -0500

    Simplify the spec

--- src/curbside/ml/utils/spec.clj
+++ src/curbside/ml/utils/spec.clj
@@ -3,20 +3,10 @@
    [clojure.spec.alpha :as s]
    [expound.alpha :as expound]))
 
-(s/def ::finite-double
-  (s/double-in :NaN? false :infinite? false))
-
 (s/def ::finite-number
-  (s/with-gen
-    (s/and
-     (s/conformer
-      (fn [x]
-        (try
-          (double x)
-          (catch Exception _e
-            ::s/invalid))))
-     ::finite-double)
-    #(s/gen ::finite-double)))
+  (s/and
+   number?
+   #(Double/isFinite %)))
 
 (defn check
   "Returns true if provided data matches spec, throws an
