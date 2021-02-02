a36b942cd90ccda8bd66faa208904ab138659d64
Alexandre Gariépy <alex.gariepy@curbside.com>
2021-02-02 10:11:32 -0500

    Add a generator to the finite number spec

--- src/curbside/ml/utils/spec.clj
+++ src/curbside/ml/utils/spec.clj
@@ -3,15 +3,20 @@
    [clojure.spec.alpha :as s]
    [expound.alpha :as expound]))
 
+(s/def ::finite-double
+  (s/double-in :NaN? false :infinite? false))
+
 (s/def ::finite-number
-  (s/and
-   (s/conformer
-    (fn [x]
-      (try
-        (double x)
-        (catch Exception _e
-          ::s/invalid))))
-    (s/double-in :NaN? false :infinite? false)))
+  (s/with-gen
+    (s/and
+     (s/conformer
+      (fn [x]
+        (try
+          (double x)
+          (catch Exception _e
+            ::s/invalid))))
+     ::finite-double)
+    #(s/gen ::finite-double)))
 
 (defn check
   "Returns true if provided data matches spec, throws an
--- test/curbside/ml/utils/spec_test.clj
+++ test/curbside/ml/utils/spec_test.clj
@@ -1,6 +1,7 @@
 (ns curbside.ml.utils.spec-test
   (:require
    [clojure.spec.alpha :as s]
+   [clojure.spec.gen.alpha :as gen]
    [clojure.test :refer [deftest is]]
    [curbside.ml.utils.spec :as spec]))
 
@@ -15,4 +16,8 @@
   (is (not (s/valid? ::spec/finite-number :asdf)))
   (is (not (s/valid? ::spec/finite-number Double/POSITIVE_INFINITY)))
   (is (not (s/valid? ::spec/finite-number Double/NEGATIVE_INFINITY)))
-  (is (not (s/valid? ::spec/finite-number Float/POSITIVE_INFINITY))))
+  (is (not (s/valid? ::spec/finite-number Float/POSITIVE_INFINITY)))
+
+  (is (s/valid? ::spec/finite-number
+                (gen/generate (s/gen ::spec/finite-number)))
+      "ensure that we can use this spec for generative testing"))
